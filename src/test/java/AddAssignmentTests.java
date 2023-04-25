41416b8b35d999ef5613e009f30d1b75fbfb3ee8
Duma Luana <likeluanalike@gmail.com>
2023-04-25 21:00:00 +0300

    Added new tests for AddTema (AddAssignment)

--- src/test/java/AddAssignmentTests.java
+++ src/test/java/AddAssignmentTests.java
@@ -50,16 +50,93 @@ public class AddAssignmentTests {
 
     @Test
     void testSaveTemaDuplicate() {
-        // Test case for trying to add an already existing student
+        // Test case for trying to add an already existing tema
         String id = "2";
         String desc = "descriere tema";
         int startWeek = 5;
         int endWeek = 6;
 
-        service.saveTema(id, desc, endWeek, startWeek); // Save the student once
+        service.saveTema(id, desc, endWeek, startWeek); // Save the tema once
 
-        int result = service.saveTema(id, desc, endWeek, startWeek); // Attempt to save the student again
+        int result = service.saveTema(id, desc, endWeek, startWeek); // Attempt to save the tema again
 
         assertEquals(1, result);
+    }
+
+    @Test
+    void testSaveTema_emptyId() {
+        // Test case for an assignment with an empty ID
+        int result = service.saveTema("", "Descriere Tema", 10, 8);
+        assertEquals(1, result, "Expected saveTema to return 1 for an assignment with an empty ID");
+    }
+
+    @Test
+    void testSaveTema_nullId() {
+        // Test case for an assignment with a null ID
+        int result = service.saveTema(null, "Descriere Tema", 10, 8);
+        assertEquals(1, result, "Expected saveTema to return 1 for an assignment with a null ID");
+    }
+
+    @Test
+    void testSaveTema_emptyDescriere() {
+        // Test case for an assignment with an empty description
+        int result = service.saveTema("1", "", 10, 8);
+        assertEquals(1, result, "Expected saveTema to return 1 for an assignment with an empty description");
+    }
+
+    @Test
+    void testSaveTema_nullDescriere() {
+        // Test case for an assignment with a null description
+        int result = service.saveTema("1", null, 10, 8);
+        assertEquals(1, result, "Expected saveTema to return 1 for an assignment with a null description");
+    }
+
+    @Test
+    void testSaveTema_invalidDeadlineIsZero() {
+        // Test case for an assignment with an invalid deadline - deadline = 0 (should be >= 1)
+        int result = service.saveTema("1", "Descriere Tema", 0, 8);
+        assertEquals(1, result, "Expected saveTema to return 1 for an assignment with an invalid deadline (< 1)");
+    }
+
+    @Test
+    void testSaveTema_invalidDeadlineAboveLimit() {
+        // Test case for an assignment with an invalid deadline - deadline = 15 (should be <= 14)
+        int result = service.saveTema("1", "Descriere Tema", 15, 14);
+        assertEquals(1, result, "Expected saveTema to return 1 for an assignment with an invalid deadline (> 14)");
+    }
+
+    @Test
+    void testSaveTema_invalidDeadlineNegative() {
+        // Test case for an assignment with an invalid deadline - deadline = -1 (should be >= 1)
+        int result = service.saveTema("1", "Descriere Tema", -1, 14);
+        assertEquals(1, result, "Expected saveTema to return 0 for an assignment with an invalid deadline (negative)");
+    }
+
+    @Test
+    void testSaveTema_invalidStartlineIsZero() {
+        // Test case for an assignment with an invalid startline (e.g., negative)
+        int result = service.saveTema("1", "Descriere Tema", 10, 0);
+        assertEquals(1, result, "Expected saveTema to return 0 for an assignment with an invalid startline (< 1)");
+    }
+
+    @Test
+    void testSaveTema_invalidStartlineAboveLimit() {
+        //Test case for an assignment with an invalid startline (e.g., >= 1000)
+        int result = service.saveTema("1", "Descriere Tema", 14, 15);
+        assertEquals(1, result, "Expected saveTema to return 0 for an assignment with an invalid startline (> 14)");
+    }
+
+    @Test
+    void testSaveTema_invalidStartlineNegative() {
+        //Test case for an assignment with an invalid startline (e.g., >= 1000)
+        int result = service.saveTema("1", "Descriere Tema", 10, -1);
+        assertEquals(1, result, "Expected saveTema to return 0 for an assignment with an invalid startline (negative)");
+    }
+
+    @Test
+    void testSaveTema_invalidStartlineMoreThanDeadline() {
+        //Test case for an assignment with an invalid deadline and startline combination (startline > deadline)
+        int result = service.saveTema("1", "Descriere Tema", 10, 11);
+        assertEquals(1, result, "Expected saveTema to return 0 for an assignment with a startline bigger than the deadline");
     }
 }
